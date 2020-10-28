f9bb974d70c0650e6cbdc76f335f21bf6c8231b5
PratikGarai <pratikgarai0208@gmail.com>
2020-10-28 18:00:56 +0530

    Added 2

---
+++ Array_And_Strings/2_checkPermutation.cpp
@@ -0,0 +1,43 @@
+// Given 2 strings , check if one is permutaion of the other
+
+#include<iostream>
+using namespace std;
+
+bool check(char* a, char* b, int m, int n)
+{
+	if(m!=n)
+		return false;
+	int alpha_count[256];
+	for(int i=0;i<256;i++)
+		alpha_count[i] = 0;
+	for(int i=0;i<m;i++)
+		alpha_count[(int)a[i]]++;
+	for(int i=0;i<m;i++)
+		alpha_count[(int)a[i]]--;
+	for(int i=0;i<256;i++)
+		if(alpha_count[i]!=0)
+			return false;
+	return true;
+}
+
+int main()
+{
+	int m,n;
+	cout<<"Enter the length of string 1 : ";
+	cin>>m;
+	char a[m];
+	cout<<"Enter string 1 : ";
+	cin>>a;
+	cout<<"Enter the length of string 2 : ";
+	cin>>n;
+	char b[n];
+	cout<<"Enter string 2 : ";
+	cin>>b;
+
+	if(check(a,b,m,n))
+		cout<<"Yes\n";
+	else 
+		cout<<"No\n";
+
+	return 0;
+}
