cc6ccfde9ddd8fab9e70ede684d68145f81a2834
PratikGarai <pratikgarai0208@gmail.com>
2020-11-11 06:41:28 +0530

    Added first method

--- LinkedLists/4_partition.java
+++ LinkedLists/4_partition.java
@@ -1,12 +1,65 @@
 // partition a linked list so that all elements less than a given number occur before the ones greater than or equal to it
+// 2 approaches are there :
+// 	1. create a new list
+// 	2. use the same list by shifting greater and equal elements to tail
 
 import java.util.Scanner;
 
 class Partition 
 {
+ 	Node method1(Node head, int partition)
+	{
+		Node curr = head;
+		Node head1 = null;
+		Node curr1 = null;
+		Node head2 = null;
+		Node curr2 = null;
+
+		while(curr!=null)
+		{
+			if(curr.data<partition)
+			{
+				if(head1==null)
+				{
+					head1 = new Node(curr.data);
+					curr1 = head1;
+				}
+				else
+				{
+					curr1.next = new Node(curr.data);
+					curr1 = curr1.next;
+				}
+			}
+			else
+			{
+				if(head2==null)
+				{
+					head2 = new Node(curr.data);
+					curr2 = head2;
+				}
+				else
+				{
+					curr2.next = new Node(curr.data);
+					curr2 = curr2.next;
+				}
+			}
+			curr = curr.next;
+		}
+
+		curr1.next = head2;
+		return head1;
+	}
+
+	void method2(Node head, int partition)
+	{
+		if(head==null || head.next==null)
+			return;
+	}
+
 	public static void main(String[] args)
 	{
 		Scanner in = new Scanner(System.in);
+		Partition ob = new Partition();
 
 		System.out.print("Enter the number of elements : ");
 		int n = in.nextInt();
@@ -29,22 +82,29 @@ class Partition
 				curr = curr.next;
 			}
 		}
-
-		System.out.print("\n The entered list is : ");
+		System.out.print("\nThe entered list is : ");
 		curr = head;
 		while(curr!=null)
 		{
-			System.out.print(curr.data);
+			System.out.print(curr.data+" ");
 			curr = curr.next;
 		}
+		
+		System.out.print("\nEnter the partition element : ");
+		int partition = in.nextInt();
 
-		System.out.print("\n The processed list is : ");
+		// method 1
+		// curr = ob.method1(head, partition);
+		// method 2
+		ob.method2(head, partition);
 		curr = head;
+		System.out.print("\nThe processed list is : ");
 		while(curr!=null)
 		{
-			System.out.print(curr.data);
+			System.out.print(curr.data+" ");
 			curr = curr.next;
 		}
+	}
 }
 
 class Node
