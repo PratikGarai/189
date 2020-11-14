08071622ae1fe58af2cdc90f349f439038fc6f0b
PratikGarai <pratikgarai0208@gmail.com>
2020-11-14 13:19:57 +0530

    Added 5

--- LinkedLists/5_sumLists.java
+++ LinkedLists/5_sumLists.java
@@ -5,7 +5,6 @@
 import java.util.Scanner;
 class SumLists
 {
-
 	Node numberToNode(int n)
 	{
 		Node head=null;
@@ -39,6 +38,79 @@ class SumLists
 		}
 		System.out.println("End");
 	}
+	
+	Node add(Node n1 , Node n2)
+	{
+		Node a = n1;
+		Node b = n2;
+		Node head = null;
+		Node curr = null;
+		int carry = 0;
+		while(a!=null && b!=null)
+		{
+			carry += a.data + b.data;
+			if(head==null)
+			{
+				head = new Node(carry%10);
+				curr = head;
+			}
+			else
+			{
+				curr.next = new Node(carry%10);
+				curr = curr.next;
+			}
+			carry = carry/10;
+			a = a.next;
+			b = b.next;
+		}
+		while(a!=null)
+		{
+			carry += a.data;
+			if(head==null)
+			{
+				head = new Node(carry%10);
+				curr = head;
+			}
+			else
+			{
+				curr.next = new Node(carry%10);
+				curr = curr.next;
+			}
+			carry = carry/10;
+			a = a.next;
+		}
+		while(b!=null)
+		{
+			carry += b.data;
+			if(head==null)
+			{
+				head = new Node(carry%10);
+				curr = head;
+			}
+			else
+			{
+				curr.next = new Node(carry%10);
+				curr = curr.next;
+			}
+			carry = carry/10;
+			b = b.next;
+		}
+		while(carry!=0)
+		{
+			if(head==null)
+			{
+				head = new Node(carry%10);
+				curr = head;
+			}
+			else
+			{
+				curr.next = new Node(carry%10);
+				curr = curr.next;
+			}
+			carry = carry/10;
+		}
+		return head;
+	}
 
 	public static void main(String[] args)
 	{
@@ -57,6 +129,9 @@ class SumLists
 		ob.printList(na);
 		System.out.print("The 2nd list is : ");
 		ob.printList(nb);
+		Node res = ob.add(na,nb);
+		System.out.print("The result list is : ");
+		ob.printList(res);
 	}
 }
 
