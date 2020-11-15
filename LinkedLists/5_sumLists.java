6ebb3ab5d07dc536aab794426c1955357f89edd8
PratikGarai <pratikgarai0208@gmail.com>
2020-11-15 12:52:50 +0530

    Added 5 follow up

--- LinkedLists/5_sumLists.java
+++ LinkedLists/5_sumLists.java
@@ -1,5 +1,5 @@
 // two linked lists have numbers in reverse order , add them to make to make a new reversed inked list
-// 7 -> 1 -> 6 + 5 -> 9-> 2 = 2 -> -1 -> 9
+// 7 -> 1 -> 6  +  5 -> 9-> 2  =  2 -> -1 -> 9
 // It means 617 + 295 = 912
 
 import java.util.Scanner;
@@ -112,6 +112,80 @@ class SumLists
 		return head;
 	}
 
+	Node numberToNodeForward(int n)
+	{
+		Node head = null;
+		Node curr = null;
+		while(n!=0)
+		{
+			curr = new Node(n%10);
+			n = n/10;
+			curr.next = head;
+			head = curr;
+		}
+		return head;
+	}
+
+	Node addFront(Node a , Node b)
+	{
+		int l1 = 0;
+		int l2 = 0;
+		Node curr = a;
+		while(curr!=null)
+		{
+			l1++;
+			curr = curr.next;
+		}
+		curr = b;
+		while(curr!=null)
+		{
+			l2++;
+			curr = curr.next;
+		}
+
+		if(l1<l2)
+		{
+			a = padWithZeroFront(a,l2-l1);
+		}
+		else if(l2<l1)
+		{
+			b = padWithZeroFront(b,l1-l2);
+		}
+
+		Node sum = new Node(0);
+		int carry = recuSum(a,b,sum);
+		if(carry!=0)
+			sum.data = carry;
+		else
+			sum = sum.next;
+		return sum;
+	}
+
+	Node padWithZeroFront(Node head, int t)
+	{
+		Node curr = null;
+		for(int i=0;i<t;i++)
+		{
+			curr = new Node(0);
+			curr.next = head;
+			head = curr;
+		}
+		return head;
+	}
+
+	int recuSum(Node a, Node b, Node prev)
+	{
+		int carry = 0;
+		prev.next = new Node(0);
+		if(a.next!=null)
+		{
+			carry = recuSum(a.next, b.next, prev.next);
+		}
+		carry += a.data+b.data;
+		prev.next.data = carry%10;
+		return carry/10;
+	}
+
 	public static void main(String[] args)
 	{
 		Scanner in = new Scanner(System.in);
@@ -123,13 +197,19 @@ class SumLists
 		int b = in.nextInt();
 		System.out.println();
 
-		Node na = ob.numberToNode(a);
-		Node nb = ob.numberToNode(b);
+		// Node na = ob.numberToNode(a);
+		// Node nb = ob.numberToNode(b);
+		// Node res = ob.add(na,nb);
+
+		// For follow up question
+		Node na = ob.numberToNodeForward(a);
+		Node nb = ob.numberToNodeForward(b);
+		Node res = ob.addFront(na,nb);
+
 		System.out.print("The 1st list is : ");
 		ob.printList(na);
 		System.out.print("The 2nd list is : ");
 		ob.printList(nb);
-		Node res = ob.add(na,nb);
 		System.out.print("The result list is : ");
 		ob.printList(res);
 	}
