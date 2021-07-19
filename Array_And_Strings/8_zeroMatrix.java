2b7edb9623267529bfdda7c08bfdd61ade119440
PratikGarai <pratikgarai0208@gmail.com>
2021-07-19 09:14:10 +0530

    zero Matrix

--- Array_And_Strings/8_zeroMatrix.java
+++ Array_And_Strings/8_zeroMatrix.java
@@ -5,6 +5,55 @@ import java.util.Scanner;
 class ZeroMatrix {
 
 	public void getZeroMatrixInplace(int[][] matrix) {
+		int m = matrix.length;
+		if(m==0)
+			return;
+		int n = matrix[0].length;
+		boolean zeroFirstRow = false, zeroFirstColumn = false;
+		
+		// Check if first rows and columns have zeros
+		for(int i=0; i<m; i++) {
+			if (matrix[i][0]==0)
+				zeroFirstColumn = true;
+		}
+		for(int i=0; i<n; i++) {
+			if (matrix[0][i]==0)
+				zeroFirstRow = true;
+		}
+
+		// If body has zero, cjange values of the first rows and columns to 0
+		for(int i=0; i<m; i++) {
+			for(int j=0; j<n; j++) {
+				if(matrix[i][j]==0){
+					matrix[0][j] = 0;
+					matrix[i][0] = 0;
+				}
+			}
+		}
+		
+		// Filling rest of the body
+		for(int i=1; i<m; i++) {
+			if(matrix[i][0]==0) {
+				for(int j=1; j<n; j++)
+					matrix[i][j] = 0;
+			}
+		}
+		for(int i=1; i<n; i++) {
+			if(matrix[0][i]==0) {
+				for(int j=1; j<m; j++)
+					matrix[j][i] = 0;
+			}
+		}
+
+		// Filling the first and second row
+		if(zeroFirstRow) {
+			for(int i=0; i<n; i++)
+				matrix[0][i] = 0;
+		}
+		if(zeroFirstColumn) {
+			for(int i=0; i<m; i++)
+				matrix[i][0] = 0;
+		}
 	}
 
 	public void printMatrix(int[][] matrix) {
